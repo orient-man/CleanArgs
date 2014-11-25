cdb9f0e775489c9ba2e24bbe8e593a494e275f8b
orientman <orientman@gmail.com>
2014-11-25 22:23:01 +0100

    Cleanup

--- FSharpArgsTests/Args.fs
+++ FSharpArgsTests/Args.fs
@@ -7,28 +7,28 @@ open Rop
 open Swensen.Unquote
 
 type SchemaElement = | Bool
-type Schema = (char * SchemaElement) list
+type SchemaInfo = (char * SchemaElement) list
 type ErrorCode = | InvalidArgumentName of char
-type SchemeParsingResult = Result<Schema, ErrorCode>
+type SchemeParsingResult = Result<SchemaInfo, ErrorCode>
 
-let parseSchema (schema: string) =
-    let rec parseList acc list =
-        match list with
-        | [] -> Success(acc)
-        | (name, param)::tail when Char.IsLetter name -> parseList ((name, param)::acc) tail
-        | (name, _)::_ -> Failure(InvalidArgumentName name)
+let rec parseSchemaElements schema = function
+    | [] -> Success(schema)
+    | (name, param)::tail when Char.IsLetter name ->
+        parseSchemaElements ((name, param)::schema) tail
+    | (name, _)::_ -> Failure(InvalidArgumentName name)
 
+let parseSchema (schema : string) : SchemeParsingResult =
     schema.Split ','
-    |> Seq.ofArray
-    |> Seq.filter (fun s -> s.Length > 0)
-    |> Seq.map (fun s -> (s.[0], s.Substring(1)))
-    |> List.ofSeq
-    |> parseList []
-    |> map (List.map (fun (name, _) -> (name, Bool)))
+    |> List.ofArray
+    |> List.map (fun s -> s.Trim())
+    |> List.filter (fun s -> s.Length > 0)
+    |> List.map (fun s -> (s.[0], s.Substring(1)))
+    |> parseSchemaElements []
+    |> Rop.map (List.map (fun (name, _) -> (name, Bool)))
 
 type ArgValue = | Flag of bool
 
-let parse (schema: string) args =
+let parse (schema : string) args =
     schema.Split ','
     |> Seq.ofArray
     |> Seq.filter (fun s -> s.Length > 0)
