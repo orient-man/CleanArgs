f05a99431d2a4774e2f29a04cb3b6bba1cea637b
orientman <orientman@gmail.com>
2014-11-25 16:55:43 +0100

    Beginning of the begining: bool flags

---
+++ FSharpArgsTests/Args.fs
@@ -0,0 +1,25 @@
+﻿module FSharpArgsTests
+
+open FsUnit
+open NUnit.Framework
+
+type ArgValue = | Flag of bool
+
+let parse (schema: string) args =
+    schema.Split ','
+    |> Seq.ofArray
+    |> Seq.filter (fun s -> s.Length > 0)
+    |> Seq.map (fun s -> (s.[0], Flag true))
+    |> List.ofSeq
+
+[<Test>]
+let ``When no schema or arguments result is empty``() =
+    parse "" [] |> should haveLength 0
+
+[<Test>]
+let ``Bool flag``() =
+    parse "x" ["x"] |> should equal [('x', Flag true)]
+
+[<Test>]
+let ``Multiple bool flags``() =
+    parse "x,y" ["x", "y"] |> should equal [('x', Flag true); ('y', Flag true)]
